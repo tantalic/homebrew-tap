bebc05e2fe721e82bbfd565c91e1608316e88535
Kevin Stock <kevinstock@tantalic.com>
2017-02-01 07:54:48 -0800

    Updates servemd to v0.6.0

--- Formula/servemd.rb
+++ Formula/servemd.rb
@@ -1,9 +1,9 @@
 class Servemd < Formula
   desc "simple HTTP server for markdown content"
   homepage "https://github.com/tantalic/servemd"
-  url "https://github.com/tantalic/servemd/releases/download/v0.5.0/servemd-darwin_amd64"
-  version "0.5.0"
-  sha256 "1a0f9f5e018898fb06105c2173e7553adbbe6845edefd7352950a67b3a997373"
+  url "https://github.com/tantalic/servemd/releases/download/v0.6.0/servemd-darwin_amd64"
+  version "0.6.0"
+  sha256 "dd42aa51aae5bb8a39237c6971c8f67acf219843ec24a8bbc6d6c862814dfb19"
 
   def install
     bin.install "servemd-darwin_amd64" => "servemd"
