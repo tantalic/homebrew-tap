5d9af841167161f5ec38b1f5f1a4d948c50f9a5c
Kevin Stock <kevinstock@tantalic.com>
2017-02-20 16:52:07 -0800

    Updates dropbox-oneway to v0.1.1

--- Formula/dropbox-oneway.rb
+++ Formula/dropbox-oneway.rb
@@ -1,9 +1,9 @@
 class DropboxOneway < Formula
   desc "one-way sync of content from Dropbox"
   homepage "https://github.com/tantalic/dropbox-oneway"
-  url "https://github.com/tantalic/dropbox-oneway/releases/download/v0.1.0/dropbox-oneway-darwin_amd64"
-  version "0.1.0"
-  sha256 "4e0a6beb0f16228f9b5570fd63e24edf854ef4345d0a28e7776988ee4c96eb14"
+  url "https://github.com/tantalic/dropbox-oneway/releases/download/v0.1.1/dropbox-oneway-darwin_amd64"
+  version "0.1.1"
+  sha256 "a87a6271f59cba96497ff8df65dc226f56d5cb34d9ac66afada42287989f653f"
 
   def install
     bin.install "dropbox-oneway-darwin_amd64" => "dropbox-oneway"
