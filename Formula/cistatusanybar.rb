b5ad317c6f5325e0fa9cf9a3d2981a1bc443f442
Kevin Stock <kevinstock@tantalic.com>
2017-04-15 14:45:15 -0700

    Updates cistatusanybar to v0.2.0

--- Formula/cistatusanybar.rb
+++ Formula/cistatusanybar.rb
@@ -1,9 +1,9 @@
 class Cistatusanybar < Formula
   desc "Add a AnyBar menu bar continuous integration status indicator"
   homepage "https://github.com/tantalic/cistatus/tree/master/cmd/cistatusanybar"
-  url "https://github.com/tantalic/cistatus/releases/download/v0.1.2/cistatusanybar-darwin_amd64"
-  version "0.1.2"
-  sha256 "e9a68f99b807288f2908b24ce43e6f1a4bb8ddd09919196d699d0309a766336e"
+  url "https://github.com/tantalic/cistatus/releases/download/v0.2.0/cistatusanybar-darwin_amd64"
+  version "0.2.0"
+  sha256 "163ea2863ee878b7b084a79b75e3fa1ea6ad3e1fa5624a0c88b4cb43583b8d43"
 
   def install
     bin.install "cistatusanybar-darwin_amd64" => "cistatusanybar"
